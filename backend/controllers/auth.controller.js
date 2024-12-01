ee0acefc5f441fdc7e4c911c66f57442381ce962
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2024-12-01 17:35:01 +0530

    fix: Indicates a bug fix. (getMe, protectRoute): Specifies the affected parts of the code. resolve promise handling and improve error checks: Briefly describes what was fixed. Optional body: Provides detailed explanations of what was done.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -92,8 +92,18 @@ export const logout = async(req,res)=>{
     res.cookie("jwt","",{maxAge:0})
     res.status(200).json({message:"Logged out Successfully"})
    } catch (error) {
-    console.log("Error in Login controller",error.message);
+    console.log("Error in Logout controller",error.message);
 
     res.status(500).json({error:"Internal server error"});
    }
+}
+export const getMe = async (req,res)=>{
+    try {
+        const user = await User.findById(req.user._id).select("-password");
+        res.status(200).json(user);
+    } catch (error) {
+        console.log("Error in getMe controller",error.message);
+
+        res.status(500).json({error:"Internal server error"});
+    }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRouts from "./routes/auth.routs.js"
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -11,6 +12,8 @@ const PORT =process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use(cookieParser());
 
 app.use("/api/auth",authRouts);
 
