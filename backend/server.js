312045a08acff85acc6367c3093ae1e1f7473141
MrutunjayPradhan <mrutyunjayapradhan678@gmail.com>
2024-12-20 10:44:05 +0530

    Adding Notification Routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,12 @@ import{v2 as cloudinary} from "cloudinary"
 import authRouts from "./routes/auth.routs.js";
 import userRouts from "./routes/user.routs.js";
 import postRouts from "./routes/post.routs.js";
+import notificationRoutes from "./routes/notification.routs.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
+
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
@@ -18,15 +21,15 @@ cloudinary.config({
 const app = express();
 const PORT =process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json());// to parse req.body
+app.use(express.urlencoded({extended:true}));// to parse form data(urlencoded)
 
 app.use(cookieParser());
 
 app.use("/api/auth",authRouts);
 app.use("/api/users",userRouts);
 app.use("/api/posts",postRouts);
-
+app.use("/api/notifications", notificationRoutes);
 
 app.get("/",(req,res)=>{
     res.send("server is redy");
